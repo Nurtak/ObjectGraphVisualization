ab084b7af95261d69dd3accd4e2f870c053a1590
arieser <arieser@PIN1206020.hsr.ch>
2015-05-25 20:48:00 +0200

    ModelManagerTest part2

--- test/ch/hsr/ogv/model/ModelManagerTest.java
+++ test/ch/hsr/ogv/model/ModelManagerTest.java
@@ -1,6 +1,9 @@
 package ch.hsr.ogv.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import javafx.geometry.Point3D;
 import javafx.scene.paint.Color;
@@ -59,7 +62,7 @@ public class ModelManagerTest {
 		mm.clearClasses();
 		assertTrue(mm.getClasses().isEmpty());
 		mm.createClass(new Point3D(-400, 0, 0), 100, 100, Color.BEIGE);
-		assertTrue(!mm.getClasses().isEmpty());
+		assertFalse(mm.getClasses().isEmpty());
 		mm.clearClasses();
 		assertTrue(mm.getClasses().isEmpty());
 	}
@@ -70,13 +73,13 @@ public class ModelManagerTest {
 		ModelManager mm = new ModelManager();
 		ModelClass modelClassA = mm.createClass(new Point3D(0, 0, 0), 100, 100, Color.BEIGE);
 		mm.deleteClass(modelClassA);
-		assertTrue(!mm.getClasses().contains(modelClassA));
+		assertFalse(mm.getClasses().contains(modelClassA));
 		ModelClass modelClassB = mm.createClass(new Point3D(-200, 0, 0), 100, 100, Color.BEIGE);
 		ModelClass modelClassC = mm.createClass(new Point3D(-400, 0, 0), 100, 100, Color.BEIGE);
 		assertTrue(mm.getClasses().contains(modelClassB));
 		assertTrue(mm.getClasses().contains(modelClassC));
 		mm.deleteClass(modelClassB);
-		assertTrue(!mm.getClasses().contains(modelClassB));
+		assertFalse(mm.getClasses().contains(modelClassB));
 		assertTrue(mm.getClasses().contains(modelClassC));
 	}
 
@@ -87,7 +90,7 @@ public class ModelManagerTest {
 		ModelClass modelClassB = mm.createClass(new Point3D(-200, 0, 0), 100, 100, Color.BEIGE);
 		Relation relationAB1 = mm.createRelation(modelClassA, modelClassB, RelationType.DIRECTED_ASSOCIATION, Color.BLACK);
 		mm.deleteClass(modelClassB);
-		assertTrue(!mm.getRelations().contains(relationAB1));
+		assertFalse(mm.getRelations().contains(relationAB1));
 	}
 
 	@Test
@@ -100,8 +103,64 @@ public class ModelManagerTest {
 		assertTrue(mm.getModelClass("A").getModelObjects().contains(modelObjectA1));
 		assertTrue(mm.getModelClass("A").getModelObjects().contains(modelObjectA2));
 		mm.deleteObject(modelObjectA1);
-		assertTrue(!mm.getModelClass("A").getModelObjects().contains(modelObjectA1));
+		assertFalse(mm.getModelClass("A").getModelObjects().contains(modelObjectA1));
 		assertTrue(mm.getModelClass("A").getModelObjects().contains(modelObjectA2));
 	}
 
+	@Test
+	public void testDeleteRelation() {
+		ModelManager mm = new ModelManager();
+		ModelClass modelClassA = mm.createClass(new Point3D(0, 0, 0), 100, 100, Color.BEIGE);
+		ModelClass modelClassB = mm.createClass(new Point3D(-200, 0, 0), 100, 100, Color.BEIGE);
+		Relation relationAB1 = mm.createRelation(modelClassA, modelClassB, RelationType.DIRECTED_ASSOCIATION, Color.BLACK);
+		mm.deleteRelation(relationAB1);
+		assertTrue(mm.getRelations().isEmpty());
+		Relation relationAB2 = mm.createRelation(modelClassA, modelClassB, RelationType.DIRECTED_ASSOCIATION, Color.BLACK);
+		Relation relationAB3 = mm.createRelation(modelClassA, modelClassB, RelationType.DIRECTED_ASSOCIATION, Color.BLACK);
+		mm.deleteRelation(relationAB2);
+		assertFalse(mm.getRelations().contains(relationAB1));
+		assertFalse(mm.getRelations().contains(relationAB2));
+		assertTrue(mm.getRelations().contains(relationAB3));
+	}
+
+	@Test
+	public void testClearRelations() {
+		ModelManager mm = new ModelManager();
+		ModelClass modelClassA = mm.createClass(new Point3D(0, 0, 0), 100, 100, Color.BEIGE);
+		ModelClass modelClassB = mm.createClass(new Point3D(-200, 0, 0), 100, 100, Color.BEIGE);
+		mm.createRelation(modelClassA, modelClassB, RelationType.DIRECTED_ASSOCIATION, Color.BLACK);
+		mm.clearRelations();
+		assertTrue(mm.getRelations().isEmpty());
+		mm.createRelation(modelClassA, modelClassB, RelationType.DIRECTED_ASSOCIATION, Color.BLACK);
+		mm.createRelation(modelClassA, modelClassB, RelationType.DIRECTED_ASSOCIATION, Color.BLACK);
+		mm.clearRelations();
+		assertTrue(mm.getRelations().isEmpty());
+	}
+
+	@Test
+	public void testGetModelClass() {
+		ModelManager mm = new ModelManager();
+		ModelClass modelClassA = mm.createClass(new Point3D(0, 0, 0), 100, 100, Color.BEIGE);
+		modelClassA.setName("A");
+		assertNotNull(mm.getModelClass("A"));
+		mm.deleteClass(modelClassA);
+		assertNull(mm.getModelClass("A"));
+		ModelClass modelClassB = mm.createClass(new Point3D(-200, 0, 0), 100, 100, Color.BEIGE);
+		assertNull(mm.getModelClass(""));
+		modelClassB.setName("");
+		assertNull(mm.getModelClass(""));
+	}
+
+	@Test
+	public void testIsClassNameTaken() {
+		ModelManager mm = new ModelManager();
+		assertFalse(mm.isClassNameTaken(""));
+		assertFalse(mm.isClassNameTaken("A"));
+		ModelClass modelClassA = mm.createClass(new Point3D(0, 0, 0), 100, 100, Color.BEIGE);
+		modelClassA.setName("A");
+		assertTrue(mm.isClassNameTaken("A"));
+		modelClassA.setName("B");
+		assertFalse(mm.isClassNameTaken("A"));
+		assertTrue(mm.isClassNameTaken("B"));
+	}
 }
