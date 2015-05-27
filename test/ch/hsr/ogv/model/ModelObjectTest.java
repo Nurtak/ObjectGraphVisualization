80329a4f73226447010eb393138638f229ac9ebf
arieser <arieser@PIN1206020.hsr.ch>
2015-05-27 21:53:34 +0200

    testing

--- test/ch/hsr/ogv/model/ModelObjectTest.java
+++ test/ch/hsr/ogv/model/ModelObjectTest.java
@@ -1,6 +1,7 @@
 package ch.hsr.ogv.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import javafx.geometry.Point3D;
 import javafx.scene.paint.Color;
 
@@ -27,6 +28,29 @@ public class ModelObjectTest {
 		modelObjectA1.addAttributeValue(attr1, attrValue1);
 		assertEquals(attrValue1, modelObjectA1.getAttributeValue(attrName1));
 		assertEquals(attrValue1, modelObjectA1.getAttributeValues().get(attr1));
+	}
+
+	@Test
+	public void testIsSuperObject() {
+		assertTrue(modelObjectA1.isSuperObject());
+	}
+
+	@Test
+	public void testChangeAttributeValue() {
+		String attrName1 = "attr1";
+		Attribute attr1 = modelClassA.createAttribute(attrName1);
+		String newAttrValue = "bli";
+		modelObjectA1.changeAttributeValue(attr1, newAttrValue);
+		assertEquals(newAttrValue, modelObjectA1.getAttributeValue(attrName1));
+	}
+
+	@Test
+	public void testChangeAttributeName() {
+		String attrName1 = "attr1";
+		Attribute attr1 = modelClassA.createAttribute(attrName1);
+		String newAttrName1 = "newAttrName1";
+		modelObjectA1.changeAttributeName(attr1, newAttrName1);
+		assertEquals(modelObjectA1.getAttributeValues().get(attr1), modelObjectA1.getAttributeValue(newAttrName1));
 	}
 
 }
