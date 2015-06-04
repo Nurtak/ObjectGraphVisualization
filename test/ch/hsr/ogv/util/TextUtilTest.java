72741cc4a2f19b0030c993a20a03c8ad497b4cb6
arieser <arieser@PIN1206020.hsr.ch>
2015-06-04 19:09:44 +0200

    Testing

--- test/ch/hsr/ogv/util/TextUtilTest.java
+++ test/ch/hsr/ogv/util/TextUtilTest.java
@@ -2,6 +2,8 @@ package ch.hsr.ogv.util;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class TextUtilTest {
@@ -18,7 +20,6 @@ public class TextUtilTest {
 		String str = "bla10";
 		String result = TextUtil.countUpTrailing(str, 42);
 		assertEquals("bla42", result);
-
 	}
 
 	@Test
@@ -26,6 +27,16 @@ public class TextUtilTest {
 		String str = "bla42";
 		String result = TextUtil.countUpTrailing(str, 10);
 		assertEquals("bla43", result);
+	}
 
+	@Test
+	public void testJoin() {
+		ArrayList<String> strings = new ArrayList<>();
+		strings.add("Donald");
+		strings.add("Dagobert");
+		strings.add("Mickey");
+		String delimiter = "-";
+		String result = TextUtil.join(strings, delimiter);
+		assertEquals(strings.get(0) + "-" + strings.get(1) + "-" + strings.get(2), result);
 	}
 }
